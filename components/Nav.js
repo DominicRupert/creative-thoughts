9a468b4145f17a27aa366272b705b5cb1f62c5e8
dominic rupert <dominicrupert@gmail.com>
2022-10-22 19:38:23 -0600

    MORE

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,18 @@
+import Link from "next/link.js";
+
 export default function Nav() {
-    return(
-        <div>
-            <h1>Nav</h1>
-        </div>
-    );
+  return (
+    <nav className=" flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">Creative Thoughts</button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        <Link href={"/auth/login"}>
+          <a className="py-2 px-4 bg-cyan-500 rounded-lg text-white font-medium ml-8">
+            Join Now
+          </a>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
