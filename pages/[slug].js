08ab26c606fbef3ca36ac8b2241c5f73bebe3e2b
dominic rupert <dominicrupert@gmail.com>
2022-11-28 19:51:01 -0700

    slug

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,19 +1,55 @@
 import Message from "../components/Message.js";
 import { useRouter } from "next/router.js";
 import { useEffect, useState } from "react";
-import {auth, db} from "../utils/firebase.js";
-import {toast} from "react-toastify";
-
-
+import { auth, db } from "../utils/firebase.js";
+import { toast } from "react-toastify";
+import { updateDoc } from "firebase/firestore";
 
 export default function Details() {
-    const router = useRouter();
-    const routeData = router.query;
-    const [message, setmessage] = useState("");
-    const [allMessage, setAllMessage] = useState([]);
+  const router = useRouter();
+  const routeData = router.query;
+  const [message, setMessage] = useState("");
+  const [allMessage, setAllMessages] = useState([]);
+  const submitMessage = async () => {
+    if (!auth.currentUser) return router.push("/auth/login");
+    if (!message){
+
+      toast.error("Please enter a message", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1000,
+      });
+      return;
+    }
+    const docRef = doc(db, "posts", routeData.id);
+    await updateDoc(docRef)
+  };
   return (
     <div>
-      <h1>hello</h1>
+      <Message {...routeData}></Message>
+      <div className="my-4">
+        <div className="flex">
+          <input
+            onChange={(e) => setMessage(e.target.value)}
+            type="text"
+            value={message}
+            placeholder="Send a message 🔊"
+            className="bg-gray-800 w-full p-2 text-white text-sm"
+          />
+          <button onClick={submitMessage} className="bg-cyan-500 text-white py-2 px-4 text-sm">
+            Submit
+          </button>
+        </div>
+        <div className="py-6">
+          <h2 className="font-bold">Comments</h2>
+          {/* {  {setAllMessages.map((message) => (
+            <div>
+              <div>
+                <img src="" alt="" />
+              </div>
+            </div>
+          ))}} */}
+        </div>
+      </div>
     </div>
   );
 }
