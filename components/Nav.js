1724daa8c7d63e4852681554632f0498427ffce8
dominic rupert <dominicrupert@gmail.com>
2022-12-29 10:23:33 -0700

    fix

--- components/Nav.js
+++ components/Nav.js
@@ -1,20 +1,19 @@
-import Link from "next/link.js";
-import { auth } from "../utils/firebase.js";
+import Link from "next/link";
+import { auth } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import React from "react";
-import Image from "next/image.js";
 
 export default function Nav() {
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+
   return (
-    <nav className=" flex justify-between items-center py-10">
+    <nav className="flex justify-between items-center py-10">
       <Link href="/">
-        <button className="text-lg font-medium">Creative Thoughts</button>
+        <button className="text-lg font-medium">Creative Minds</button>
       </Link>
       <ul className="flex items-center gap-10">
         {!user && (
           <Link href={"/auth/login"}>
-            <a className="py-2 px-4 bg-cyan-500 rounded-lg text-white font-medium ml-8">
+            <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
               Join Now
             </a>
           </Link>
@@ -22,14 +21,13 @@ export default function Nav() {
         {user && (
           <div className="flex items-center gap-6">
             <Link href="/post">
-              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-lg textx-sm">
+              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg textx-sm">
                 Post
               </button>
             </Link>
             <Link href="/dashboard">
               <img
                 className="w-12 rounded-full cursor-pointer"
-                // @ts-ignore
                 src={user.photoURL}
               />
             </Link>
